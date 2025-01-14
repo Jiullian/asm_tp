5757bf47c2c7b77ce191118496597789caa74f63
Jiullian Ferreira <jiullian77470@gmail.com>
2025-01-14 08:50:52 +0100

    feat -fix negative numbers parameters

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -6,19 +6,27 @@ buffer resb 32
 section .text
 
 _start:
+    mov rax, [rsp]
+    cmp rax, 3
+    jl _pas_arguments
+
+
     mov rdi, [rsp+16]
     call atoi
     mov r8, rax
+
     mov rdi, [rsp+24]
     call atoi
     add r8, rax
     mov rdi, r8
     call itoa
+
     mov rdx, rax
     mov rax, 1
     mov rdi, 1
     mov rsi, buffer
     syscall
+
     mov rax, 60
     xor rdi, rdi
     syscall
@@ -27,16 +35,29 @@ atoi:
     xor rax, rax
     xor rcx, rcx
     xor rdx, rdx
-atoi_loop:
+
+    mov dl, [rdi]
+    cmp dl, '-'
+    jne .parse_loop
+    inc rdi
+    mov rcx, 1
+
+.parse_loop:
     mov dl, [rdi]
     cmp dl, 0
-    je atoi_done
+    je .done
     sub dl, '0'
-    imul rax, 10
+    imul rax, rax, 10
     add rax, rdx
     inc rdi
-    jmp atoi_loop
-atoi_done:
+    jmp .parse_loop
+
+.done:
+    cmp rcx, 1
+    jne .end
+    neg rax
+
+.end:
     ret
 
 itoa:
@@ -44,24 +65,35 @@ itoa:
     mov rbx, buffer + 31
     mov byte [rbx], 10
     dec rbx
-    cmp rdi, 0
-    jge itoa_positive
-    neg rdi
-itoa_positive:
+
     xor rcx, rcx
-itoa_loop:
+    test rax, rax
+    jge .positive
+    neg rax
+    mov rcx, 1
+
+.positive:
+
+.itoa_loop:
     xor rdx, rdx
-    mov rcx, 10
-    div rcx
+    mov r8, 10
+    div r8
     add rdx, '0'
     mov [rbx], dl
     dec rbx
-    cmp rax, 0
-    jne itoa_loop
-    cmp rdi, 0
-    jge itoa_done
+    test rax, rax
+    jne .itoa_loop
+
+    cmp rcx, 1
+    jne .done
     mov byte [rbx], '-'
     dec rbx
-itoa_done:
+
+.done:
     lea rax, [rbx+1]
     ret
+
+_pas_arguments:
+    mov rax, 60
+    mov rdi, 1
+    syscall
